423ce909b4385e68fdc382ea95112cc8276eb15e
Eric Johnson <ejohnson@showpitch.com>
2016-02-22 09:25:57 -0700

    Update to 0.0.3

--- dist/system/notification/notification.js
+++ dist/system/notification/notification.js
@@ -1,11 +1,11 @@
 System.register([], function (_export) {
-  'use strict';
+  "use strict";
 
   var Notification;
 
-  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+  var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
   return {
     setters: [],
@@ -16,16 +16,16 @@ System.register([], function (_export) {
         }
 
         _createClass(Notification, [{
-          key: 'send',
-          value: function send() {
-            alert('hey');
+          key: "isTrue",
+          value: function isTrue() {
+            return true;
           }
         }]);
 
         return Notification;
       })();
 
-      _export('Notification', Notification);
+      _export("Notification", Notification);
     }
   };
 });
--- src/notification/notification.js
+++ src/notification/notification.js
@@ -3,8 +3,8 @@
  */
 export class Notification {
 
-  send() {
-    alert('hey');
+  isTrue() {
+    return true;
   }
 
 }
